acc94387f946df256ecd8313e186c4d9ab50be12
Zero <blackdragon21122000@gmail.com>
2020-12-21 13:12:31 +0700

    Update

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var total_products = [];
 var totalProducts;
 var related_products = [];
 var lasted_products = [];
+var purchased_P = JSON.parse(localStorage.getItem('users')) || [];
 function returnHomePage() {
 	window.location = "mainpage.html";
 }
@@ -35,9 +36,11 @@ function renderProductList(products) {
 			"<div class='card' style='width: 18rem;'>" +
 			`<img class='card-img-top' src="${item.media.link[0]}" alt='image'>` +
 			"<div class='card-body'>" +
-			`<a href="detail.html?id=${item.productID}"><h5 class='card-title'>${item.productName}</h5></a>` +
+			`<p class='card-text'>${item.productLabel}</p>` +
+			`<hr style="border-top: 3px dashed #bbb;" >` +
+			`<a href="detail.html?id=${item.productID}"><h5 style="font-size: 13pt;" class='card-title'>${item.productName}</h5></a>` +
 			`<p class='card-text'>Color: ${item.productColor}</p>` +
-			`<p class='card-text'>Price: ${item.productPrice}</p>` +
+			`<b class='card-text'>Price: ${item.productPrice}</b>` +
 			"</div>" +
 			"</div>" +
 			"</div>"
@@ -87,10 +90,10 @@ function clickImgs(imgs) {
 	expandImg.parentElement.style.display = "block"
 }
 
-// Khi nhấn trừ số lương
+// Khi nhấn trừ số lương, size
 $(".minus_plus").on("click", function () {
 	var $button = $(this);
-	var oldValue = $button.closest('.input-counter1').find("input.counter-btn").val();
+	var oldValue = $button.closest('.input-counter').find("input.counter-btn").val();
 	if ($button.text() == "+") {
 		var newVal = parseFloat(oldValue) + 1;
 	}
@@ -102,7 +105,7 @@ $(".minus_plus").on("click", function () {
 			newVal = 1;
 		}
 	}
-	$button.closest('.input-counter1').find("input.counter-btn").val(newVal);
+	$button.closest('.input-counter').find("input.counter-btn").val(newVal);
 })
 
 // Tạo Pagination
@@ -125,16 +128,10 @@ function createPagination(total, limit) {
 			console.log(pageItem);
 			$(".pagination li").removeClass("active");
 			$(this).addClass("active");
-			$("#product_list").hide();
-			var grandTotal = limit * pageItem;
-			console.log(grandTotal);
-			for (var i = grandTotal - limit; i < grandTotal; i++) {
-				$("#product_list:eq(" + i + ")").show();
-			}
+			getData(pageItem);
 		}
 	})
 }
-
 
 /// Khi trang home được load
 $(document).ready(function () {
@@ -153,6 +150,64 @@ $(document).ready(function () {
 			getData(i - 1);
 			i -= 1;
 		}
+	})
+	// Filter
+	$('#all_pk').click(() => {
+		let filtered_product = total_products.filter(x => x.category == 'Phụ kiện');
+		renderProductList(filtered_product);
+	})
+	$('#newpk').click(() => {
+		let filtered_product = total_products.filter(x => x.category == 'Phụ kiện' && x.productLabel == 'New Arrival');
+		renderProductList(filtered_product);
+	})
+	$('#cap').click(() => {
+		let filtered_product = total_products.filter(x => x.category == 'Phụ kiện' && x.productName.includes('Cap') == true);
+		renderProductList(filtered_product);
+	})
+	$('#socks').click(() => {
+		let filtered_product = total_products.filter(x => x.category == 'Phụ kiện' && x.productName.includes('Socks') == true);
+		renderProductList(filtered_product);
+	})
+	$('#tee').click(() => {
+		let filtered_product = total_products.filter(x => x.category == 'Nửa trên' && x.productName.includes('Tee') == true);
+		renderProductList(filtered_product);
+	})
+	$('#filter_all').click(() => {
+		let filtered_product = total_products.filter(x => x.category == 'Giày');
+		renderProductList(filtered_product);
+	})
+	$('#filter_new').click(() => {
+		let filtered_product = total_products.filter(x => x.productLabel == 'New Arrival' && x.category == 'Giày');
+		renderProductList(filtered_product);
+		$('.pagination').hide();
+	})
+	$('#filter_training').click(() => {
+		let filtered_product = total_products.filter(x => x.productName.substr(0, 6) == 'Vintas');
+		renderProductList(filtered_product);
+	})
+	$('#filter_sneaker').click(() => {
+		let filtered_product = total_products.filter(x => x.productName.substr(0, 6) == 'Ananas' && x.category == 'Giày');
+		renderProductList(filtered_product);
+	})
+	$('#filter_sandal').click(() => {
+		let filtered_product = total_products.filter(x => x.productName.includes('Pattas') == true && x.category == 'Giày');
+		renderProductList(filtered_product);
+	})
+	$('#filter_boots').click(() => {
+		let filtered_product = total_products.filter(x => x.productName.substr(0, 5) == 'Basas' && x.category == 'Giày');
+		renderProductList(filtered_product);
+	})
+	$('#filter_sport').click(() => {
+		let filtered_product = total_products.filter(x => x.productName.substr(0, 6) == 'Vintas' && x.category == 'Giày');
+		renderProductList(filtered_product);
+	})
+	$('#filter_hh').click(() => {
+		let filtered_product = total_products.filter(x => x.productName.substr(0, 5) == 'Urbas' && x.category == 'Giày');
+		renderProductList(filtered_product);
+	})
+	$('#filter_doll').click(() => {
+		let filtered_product = total_products.filter(x => x.productName.includes('Low Top') == true && x.category == 'Giày');
+		renderProductList(filtered_product);
 	})
 	$('#filter_price_1').click(() => {
 		let filtered_product = total_products.filter(x => x.productPrice == '550.000 VND');
@@ -179,13 +234,102 @@ $(document).ready(function () {
 		let filtered_product = total_products.filter(x => x.productName.substr(0, 6) == 'Vintas');
 		renderProductList(filtered_product);
 	})
+	$('#filter_sales').click(() => {
+		let filtered_product = total_products.filter(x => x.productName.productLabel == 'Sales off');
+		renderProductList(filtered_product);
+	})
 });
+function loadCart() {
+	if (localStorage.getItem('cart')) {
+		return JSON.parse(localStorage.getItem('cart'));
+	}
+}
+
+function saveCart(cart) {
+	if (cart) {
+		return localStorage.setItem('cart', JSON.stringify(cart));
+	}
+	return false;
+}
+
 // Khi trang chi tiết sản phẩm load
 $('#product-info').ready(function () {
 	const urlParams = new URLSearchParams(window.location.search);
 	const product_id = urlParams.get('id');
 	showDetail(product_id);
+
+	var cart = loadCart() || []; // Mảng chứa các item có trong cart | load ra từ local hoặc [] nếu chưa có
+	$('#add_cart').click(function () {
+		let item = {
+			id: '',
+			name: '',
+			image: '',
+			price: 0
+		};
+		item.id = product_id;
+		item.name = $('.product-detail__content > h3').text();
+		item.image = $('#product__picture .picture__container img').attr('src');
+		item.price = parseInt($('.product-detail__content > .price .new__price').text().replace(',', '.').replace(' ', '').slice(0, -4));
+		// kiểm tra trong cart có sp này chưa
+		// tìm index của item trùng
+		let duplicate_index = cart.findIndex(cart_item => cart_item.product.id === item.id);
+		if (duplicate_index > -1) { // nếu trùng
+			cart[duplicate_index].quantity += parseInt($('#product_qty').val());
+		} else { // ko trùng
+			cart.push({
+				product: item,
+				quantity: parseInt($('#product_qty').val())
+			});
+		}
+		// lưu cart vào local
+		saveCart(cart);
+	})
+});
+// Khi trang cart load
+$('cart-container').ready(function () {
+	displayCart();
 })
+
+function displayCart() {
+	let cart = loadCart();
+	let total_price = 0;
+	cart.forEach(item => total_price += item.product.price * item.quantity);
+	$('.new__price').text(total_price.toString() + 'VND');
+	for (let item of cart) {
+		$('.cart-items').append(
+			`
+			<tr class="cake-top">
+				<td class="product__thumbnail">
+					<a href="#">
+						<img style="width:100%" src="${item.product.image}" alt="item image">
+					</a>
+				</td>
+				<td class="product__name">
+					<a href="#">${item.product.name}</a>
+					<br><br>
+					<small>White/6.25</small>
+				</td>
+				<td class="quantity">
+					<div class="product-right">
+						<input size="5" min="1" type="number" id="quantity" name="quantity"
+							value="${item.quantity}" class="form-control input-small">
+					</div>
+				</td>
+				<td class="price">
+					<h4>${item.product.price} VND</h4>
+				</td>
+				<td class="top-remove">
+					<h4>${item.quantity * item.product.price} VND</h4>
+					<div class="close">
+						<h5>Remove</h5>
+					</div>
+				</td>
+			</tr>
+			`
+		)
+	}
+
+}
 
 // Related__P
 async function relatedProducts() {
@@ -241,3 +385,12 @@ async function lastedProducts() {
 		)
 	});
 }
+
+// Cart
+// function addToCart() {
+// 	let purchased_P = {
+// 		name: productName,
+// 		price: productPrice,
+// 		quantity: 
+// 	}
+// }
